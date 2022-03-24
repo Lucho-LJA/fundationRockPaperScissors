35a10f6f7b90550a7292bada153b0be5ac9ce5e4
Lucho-LJA <ljallauca.a@gmail.com>
2022-03-23 23:17:09 -0500

    add js script to animation a correct id of css file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+//variables css
+const inputs = document.querySelectorAll('input');
+const wonGames = document.querySelector('#result-won');
+const lostGames = document.querySelector('#result-lost');
+const tiedGames = document.querySelector('#result-tied');
+const finalGame = document.querySelector('#general-result');
+const infoFinalResult = document.querySelector('#info-result')
+const finalResults = document.querySelector('#final-results');
+const currentResult = document.querySelector('#current-result');
+finalResults.style.visibility = 'hidden';
+finalGame.style.visibility = 'hidden';
+infoFinalResult.style.visibility = 'hidden';
+//general variables
+let contGames=0;
+let contGamePlayer=0;
+let contGameComputer=0;
+let contGameTies=0;
+let resultGame=[];
+
+
+
 function computerPlay(){
     let option
     option=Math.floor(Math.random() * 3) + 1;
@@ -31,43 +52,91 @@ function playRound(playerSelection, computerSelection) {
         return ['-100','Invalid value']
     }
   }
-function game(){
+function game(optionGame){
     let playerSelection;
-    let cont=0;
-    let resultGame;
-    let contGamePlayer=0;
-    let contGameComputer=0;
-    let contGameTies=0;
-    for (let i = 0; i < 5; i++){
-        cont=1;
-        computerSelection=computerPlay();
-        while (cont==1){
-            playerSelection=prompt("Select: Rock, Paper, or Scissors:\t");
-            console.log(computerSelection);
-            resultGame=playRound(playerSelection, computerSelection);
-            if (resultGame[0]==-100){
-                cont=1;
-            }else{
-                cont=0;
-            }
-            alert(resultGame[1]);
-        }
+    let computerSelection = computerPlay();
+    switch (optionGame) {
+        case "button-rock":
+            playerSelection="rock";
+            break;
+        case "button-paper":
+            playerSelection="paper";
+            break;
+        case "button-scissors":
+            playerSelection="scissors";
+            break;
+    
+        default:
+            playerSelection="rock";
+            break;
+    }
+    resultGame=playRound(playerSelection, computerSelection);
+    if (resultGame[0]==-100){
+        alert(resultGame[1]);
+    }else{
+        contGames++;
+        currentResult.textContent = resultGame[1];
         if (Number(resultGame[0])>0){
             contGamePlayer++;
+            currentResult.style.color = 'white';
+            currentResult.style.backgroundColor='green';
         }else if (Number(resultGame[0])<0){
             contGameComputer++;
+            currentResult.style.color = 'white';
+            currentResult.style.backgroundColor='red';
         }else{
             contGameTies++;
-        }   
+            currentResult.style.color = 'black';
+            currentResult.style.backgroundColor='yellow';
+        }
     }
-    if (contGamePlayer > contGameComputer){
-        alert(`You Win. \n${contGameTies} Ties.\n${contGamePlayer} Won.\n${contGameComputer} Lost`);
-    }else if (contGamePlayer < contGameComputer){
-        alert(`You Lose. \n${contGameTies} Ties.\n${contGamePlayer} Won.\n${contGameComputer} Lost`);
+    if (contGames>=5){
+        finalResults.style.visibility = 'visible';
+        finalGame.style.visibility = 'visible';
+        infoFinalResult.style.visibility = 'visible';
+        
+        wonGames.textContent = `${contGamePlayer}`
+        lostGames.textContent = `${contGameComputer}`;
+        tiedGames.textContent = `${contGameTies}`;
+        finalGame.style.borderStyle='solid'
+        if (contGamePlayer > contGameComputer){
+            finalGame.textContent = "YOU WIN";
+            finalGame.style.color = 'white';
+            finalGame.style.backgroundColor='green';
+        }else if (contGamePlayer < contGameComputer){
+            finalGame.textContent = "YOU LOSE";
+            finalGame.style.color = 'white';
+            finalGame.style.backgroundColor='red';
+        }else{
+            finalGame.textContent = "YOU TIE";
+            finalGame.style.color = 'black';
+            finalGame.style.backgroundColor='yellow';
+        }
+        finalGame.style.borderStyle = 'none';
+
+        contGames=0;
+        contGamePlayer=0;
+        contGameComputer=0;
+        contGameTies=0;
+        resultGame=[];
     }else{
-        alert(`You Tie. \n${contGameTies} Ties.\n${contGamePlayer} Won.\n${contGameComputer} Lost`);
+        finalResults.style.visibility = 'hidden';
+        finalGame.style.visibility = 'hidden';
+        infoFinalResult.style.visibility = 'hidden';
     }
+    
+          
+    
+
+    
 }
-game();
+
+inputs.forEach((input) => {
+
+    input.addEventListener('click', () => {
+      game(input.id);
+    });
+  });
+//game();
 
 
